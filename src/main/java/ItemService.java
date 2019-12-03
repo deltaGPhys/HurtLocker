4806277e33e89b98862ee20c9505141512b9d033
deltagphys <cty.jgates@gmail.com>
2019-12-02 20:34:13 -0500

    cleanup

--- src/main/java/ItemService.java
+++ src/main/java/ItemService.java
@@ -25,7 +25,13 @@ public class ItemService {
                 fields[i] = matcher.group(0);
 
             }
-            this.items.add(new Item(DeL33t.translate(fields[0]),(!fields[1].equals(""))?Double.parseDouble(fields[1]):null,fields[2]));
+            this.items.add(
+                new Item(
+                    DeL33t.translate(fields[0]),
+                    (!fields[1].equals("")) ? Double.parseDouble(fields[1]) : null,
+                    fields[2]
+                )
+            );
         }
     }
 
@@ -53,8 +59,13 @@ public class ItemService {
         TreeSet<String> items = new TreeSet(this.inventory.keySet().stream().filter(x -> !x.getName().equals("Error")).map(x-> x.getName()).collect(Collectors.toSet()));
         String invReport = "";
         for (String name : items) {
-            Item[] pricePoints = this.inventory.keySet().stream().filter(x-> x.getName().equals(name)).collect(Collectors.toList()).toArray(new Item[0]);
-            int totalNum = Arrays.stream(pricePoints).mapToInt(x -> this.inventory.get(x)).sum();
+            Item[] pricePoints = this.inventory.keySet().stream()
+                    .filter(x-> x.getName().equals(name))
+                    .collect(Collectors.toList())
+                    .toArray(new Item[0]);
+            int totalNum = Arrays.stream(pricePoints)
+                    .mapToInt(x -> this.inventory.get(x))
+                    .sum();
             String header = "";
                 header = String.format("name: %1$8s       seen: %2$2d time"+((totalNum > 1)?"s":"")+"\n", name, totalNum);
             invReport += header;
@@ -70,7 +81,7 @@ public class ItemService {
         }
 
         // Null object pattern FTW :)
-        int totalNum = this.inventory.get(new Item(null,null,null));
+        int totalNum = this.inventory.get(new Item());
         invReport += String.format("Errors               seen: %1$2d time"+((totalNum > 1)?"s":"")+"\n",  totalNum);
         return invReport;
     }
